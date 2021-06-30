eea1823bc3198fb8281f54adcf6f1cc6b1890013
isthatsahil <sahil.verma997@gmail.com>
2021-06-30 19:35:49 +0530

    cms

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@ const parse = require("pg-connection-string").parse;
 
 module.exports = ({ env }) => {
 
+  
   if (env("NODE_ENV") === "production") {
     const config = parse(process.env.DATABASE_URL);
     return {
